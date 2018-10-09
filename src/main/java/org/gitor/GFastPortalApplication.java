9b7c13640a5a7a70aa462e8d51e2b4c32c24d034
1@gitor.org <1@gitor.org>
2018-10-09 21:40:42 +0800

    first commit

---
+++ src/main/java/org/gitor/GFastPortalApplication.java
@@ -0,0 +1,12 @@
+package org.gitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GFastPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GFastPortalApplication.class, args);
+	}
+}
